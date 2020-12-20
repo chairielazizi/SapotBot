9b037d99d61a637aa50ebf5d794a14a1a262ef5f
Chairiel Azizi <azizihr2000@gmail.com>
2020-12-20 09:16:21 +0000

    detect any sad words and send encouragements

--- main.py
+++ main.py
@@ -30,8 +30,13 @@ async def on_message(message):
   if message.author == client.user:
     return
 
+  msg = message.content
+
   if message.content.startswith("hmm"):
     quote = get_quote()
     await message.channel.send(quote)
+
+  if any(word in msg for word in sad_words):
+    await message.channel.send(random.choice(starter_encouragements))
 
 client.run(os.getenv("TOKEN"))
